2ca065dbdbe2ad650bf8751d133ce9c7dc2fa92a
wwwbbb8510 <wwwbbb8510@gmail.com>
2018-01-04 17:46:57 +1300

    fix the wrong test training dataset path

--- ippso/data/convex.py
+++ ippso/data/convex.py
@@ -1,7 +1,7 @@
 from .core import DataLoader
 import os
 
-train_path = os.path.join('convex', '50k', 'convex_test.amat')
-test_path = os.path.join('convex', 'convex_train.amat')
+train_path = os.path.join('convex', 'convex_train.amat')
+test_path = os.path.join('convex', '50k', 'convex_test.amat')
 
 loaded_data = DataLoader.load(train_path=train_path, test_path=test_path, train_validation_split_point=6000)
--- ippso/data/mdrbi.py
+++ ippso/data/mdrbi.py
@@ -1,7 +1,7 @@
 from .core import DataLoader
 import os
 
-train_path = os.path.join('mnist_rotation_back_image_new', 'mnist_all_background_images_rotation_normalized_test.amat')
-test_path = os.path.join('mnist_rotation_back_image_new', 'mnist_all_background_images_rotation_normalized_train_valid.amat')
+train_path = os.path.join('mnist_rotation_back_image_new', 'mnist_all_background_images_rotation_normalized_train_valid.amat')
+test_path = os.path.join('mnist_rotation_back_image_new', 'mnist_all_background_images_rotation_normalized_test.amat')
 
 loaded_data = DataLoader.load(train_path=train_path, test_path=test_path)
