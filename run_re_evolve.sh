3c937323ea0119a833badc227b66c604a2f8b8a8
wwwbbb8510 <wwwbbb8510@gmail.com>
2018-01-12 12:08:44 +1300

    add input arguments for re-evolution - add dataset into the log name

--- run_re_evolve.sh
+++ run_re_evolve.sh
@@ -21,5 +21,5 @@ fi
 
 EPOCH=10
 for i in `seq 1 ${EPOCH}`;do
-    python3 main.py -d ${DATASET} -m 1 --partial_dataset 0.1 --ip_structure 2 --re_evolve 1 --w 0.7298 --c1 1.49618,1.49618 --c2 1.49618,1.49618 -v 4,25.6 -s 30 -l 8 --max_steps 10 -e 10 -f ${GPU} -g 1 --log_file=log/ippso_cnn_mbi_${ID}.log --gbest_file=log/gbest_mbi_${ID}.pkl
+    python3 main.py -d ${DATASET} -m 1 --partial_dataset 0.1 --ip_structure 2 --re_evolve 1 --w 0.7298 --c1 1.49618,1.49618 --c2 1.49618,1.49618 -v 4,25.6 -s 30 -l 8 --max_steps 10 -e 10 -f ${GPU} -g 1 --log_file=log/ippso_cnn_${DATASET}_${ID}.log --gbest_file=log/gbest_${DATASET}_${ID}.pkl
 done
