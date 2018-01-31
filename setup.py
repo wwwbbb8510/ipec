fa20d23cd8edba5908e65a0ab0ab990d7ce3d5d5
wwwbbb8510 <wwwbbb8510@gmail.com>
2018-02-01 09:06:27 +1300

    add pip config files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'ipec',
+  packages = ['ipec'], # this must be the same as the name above
+  version = '0.1',
+  description = 'IP-base Evolutionary Computation',
+  author = 'Bin Wang',
+  author_email = 'wwwbbb8510@gmail.com',
+  url = 'https://github.com/wwwbbb8510/ipec.git', # use the URL to the github repo
+  download_url = 'https://github.com/wwwbbb8510/ipec/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['Evolutionary Computation', 'PSO', 'Genetic Algorithm', 'Differential Evolution'], # arbitrary keywords
+  classifiers = [],
+)
